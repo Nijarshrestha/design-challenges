cff863b5dbfdd27c9e8d545f85761d6be8bab51e
Nijarshrestha <shresthanijar@gmail.com>
2023-07-25 16:10:06 +0545

    chore: use button element instead of div

--- week-seven/src/components/buttons/MediaPlayer/MediaPlayer.svelte
+++ week-seven/src/components/buttons/MediaPlayer/MediaPlayer.svelte
@@ -9,7 +9,7 @@
     }
 </script>
 
-<div class="media-player-btn-wrapper" on:click={handleMediaPlay} role="none" style={`width: ${mediaPlay? '160px': '98px'}`}>
+<button class="media-player-btn-wrapper" on:click={handleMediaPlay} aria-hidden="true" style={`width: ${mediaPlay? '160px': '98px'}`}>
     {#if mediaPlay}
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm-2 14c-.55 0-1-.45-1-1V9c0-.55.45-1 1-1s1 .45 1 1v6c0 .55-.45 1-1 1zm4 0c-.55 0-1-.45-1-1V9c0-.55.45-1 1-1s1 .45 1 1v6c0 .55-.45 1-1 1z"/></svg>
     <span>  
@@ -35,10 +35,11 @@
             5:23
         </span>   
     {/if }  
-</div>
+</button>
 
 <style lang="scss">
     .media-player-btn-wrapper{
+        all: unset;
         border-radius: 50px;
         color: white;
         background-color: #16161d;
